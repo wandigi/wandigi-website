0bda9e4bfcb3280e6d5b56f425ae2ad5b9f6ed0e
WanDigi <wandigi.id@gmail.com>
2026-05-01 05:39:45 +0000

    adminn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  // WAJIB: Mengubah output menjadi 'server' agar bisa menjalankan logic di server
+  output: 'server', 
+  
   vite: {
     plugins: [tailwindcss()]
   }
